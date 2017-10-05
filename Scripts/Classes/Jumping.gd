0a607dc613fed8464c847c3f502f779ec60b61fe
AndyGFX <o.kollar@gmail.com>
2017-10-05 14:37:07 +0200

    Fix

--- Scripts/Classes/Jumping.gd
+++ Scripts/Classes/Jumping.gd
@@ -21,27 +21,49 @@ func SetGravity(new_gravity):
 	
 	
 func Apply(delta):
-			
-	# add gravity
+	
+#	# add gravity
+#	velocity +=  world_gravity * delta
+#	
+#	
+#	#_jump_timer += delta 
+#	
+#	object.move_and_slide(velocity,Vector2(0, -1),20)
+#	_is_ground = object.is_move_and_slide_on_floor()
+#	
+#	# Is on ground ?
+#	if _is_ground: 
+#		_jump_timer = 0
+#		velocity.y = 0
+#	
+#	# is jump enabled by treshold time ?
+#	_is_enabled = _jump_timer < jumpTreshold 
+#	
+#	# Apply jump force on key pressed when is enabled
+#	if( key.Check() and _is_enabled):
+#		velocity.y -= jumpForce
+#		_jump_timer = jumpTreshold 
+
 	velocity +=  world_gravity * delta
-	
-	
-	#_jump_timer += delta 
-	
+
 	object.move_and_slide(velocity,Vector2(0, -1),20)
 	_is_ground = object.is_move_and_slide_on_floor()
 	
-	print(_is_ground)
-	
 	# Is on ground ?
-	if _is_ground: 
+	if _is_ground:
+		_is_enabled = true
 		_jump_timer = 0
 		velocity.y = 0
+		
+		if key.Check() and _is_enabled: 
+			velocity.y -= jumpForce
+			_jump_timer = jumpTreshold 
+			_is_enabled = false
 	
-	# is jump enabled by treshold time ?
-	_is_enabled = _jump_timer < jumpTreshold 
+		
+		
+		
 	
-	# Apply jump force on key pressed when is enabled
-	if( key.Check() and _is_enabled):
-		velocity.y -= jumpForce
-		_jump_timer = jumpTreshold 
+	
+	
+	
