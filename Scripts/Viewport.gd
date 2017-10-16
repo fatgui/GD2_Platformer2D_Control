843f632864ec2d43f7896af3d92ef71c52d320d9
AndyGFX <o.kollar@gmail.com>
2017-10-16 19:42:38 +0200

    ADD: comments to code

--- Scripts/Viewport.gd
+++ Scripts/Viewport.gd
@@ -1,8 +1,5 @@
-
 extends Node2D
 
-
-# Getting globals (values from the Scene menu -> Project Settings). An alternative is OS.get_window_size()
 onready var screen_size = Vector2(Globals.get("display/width"), Globals.get("display/height")) 
 onready var player = get_node("Player")
 onready var HUD = get_node("HUD")
@@ -14,19 +11,26 @@ var old_pos
 var new_pos
 var canvas_transform
 
+# ---------------------------------------------------------------------------------
+# Initialize on start
+# ---------------------------------------------------------------------------------
 func _ready():
+	
 	# Initializes the camera, sets its position to that of the player
 	update_camera()
-	# Instead of connecting the signal from the editor, we can do this via code. We connect from the player (using the player variable) to the camera (the self argument below).
-	# However, I prefer to do this in the editor as it'll display an icon, making it easy to spot connections later
+	
+	# Connect player to update_camera via signal directly from script
 	player.connect("moveSignal", self, "update_camera")
+	
+	# enable smooth follow camera
 	if smoothFollow: set_process(true)
 	
+# ---------------------------------------------------------------------------------
 # Center the camera on the player. Automatically called when the player moves.
+# ---------------------------------------------------------------------------------
 func update_camera():
+
 	canvas_transform = get_viewport().get_canvas_transform()
-	# canvas_transform is of type Matrix32. That's an array of 3 Vector2, and canvas_transform[2] controls the canvas's offset.
-	# Moving a camera isn't moving a camera per se, instead we move the canvas (all the sprites) under the camera. That's why we use -1*player.get_pos()
 	old_pos = canvas_transform[2]
 	new_pos = -player.get_pos() + screen_size / 2	
 	
@@ -35,12 +39,13 @@ func update_camera():
 		canvas_transform[2].y = new_pos.y
 		get_viewport().set_canvas_transform(canvas_transform)
 		HUD.set_pos(-canvas_transform[2])
-	
+
+# ---------------------------------------------------------------------------------
+# Smooth follow the camera on the player. Called when smoothFollow is enable (true).
+# ---------------------------------------------------------------------------------
 func _process(delta):
 
 	canvas_transform = get_viewport().get_canvas_transform()
-	# canvas_transform is of type Matrix32. That's an array of 3 Vector2, and canvas_transform[2] controls the canvas's offset.
-	# Moving a camera isn't moving a camera per se, instead we move the canvas (all the sprites) under the camera. That's why we use -1*player.get_pos()
 	old_pos = canvas_transform[2]
 	new_pos = -player.get_pos() + screen_size / 2	
 		
